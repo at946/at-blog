21e95be053b2df9f28ce6759ca64b2f0556fadc7
Asato TAKAHASHI <asato730@gmail.com>
2024-01-28 20:51:03 +0900

    add 2023-11-22 post

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,14 +5,8 @@ const blogCollection = defineCollection({
 		title: z
 			.string()
 			.max(100, "The title length must be less than or equal to 100 chars"),
-		description: z.string(),
 		tags: z.array(z.string()),
-		author: z.string().optional(),
-		authorImage: z.string().optional(),
-		authorTwitter: z.string().optional(),
 		date: z.string(),
-		image: z.string().optional(),
-		category: z.string().optional(),
 	}),
 });
 
