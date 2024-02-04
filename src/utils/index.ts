b0be863fd05c7de79d251f9d8a2e797896d16562
Asato TAKAHASHI <asato730@gmail.com>
2024-02-04 15:37:44 +0900

    install tailwindcss/nesting and remove unused toTitleCase func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,11 +14,6 @@ const MONTHS = [
 	"Dec",
 ];
 
-export const toTitleCase = (str: string) =>
-	str.replace(/\w\S*/g, function (txt) {
-		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-	});
-
 export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()];
 
 export const getSlugFromPathname = (pathname: string) =>
