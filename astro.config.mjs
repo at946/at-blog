9d58814959b0c38be792860a72a2ce1d2c9ed069
Asato TAKAHASHI <asato730@gmail.com>
2024-02-07 20:41:35 +0900

    convert some articles md to mdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,6 +36,14 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
     remarkPlugins: [remarkCodeTitles, remarkMath],
     rehypePlugins: [[rehypeKatex, {}]]
   },
+  markdoc: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'css-variables'
+    },
+    remarkPlugins: [remarkCodeTitles, remarkMath],
+    rehypePlugins: [[rehypeKatex, {}]]
+  },
   integrations: [
     astroExpressiveCode(),
     mdx(), 
