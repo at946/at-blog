1ea5db73de1ff239c8916c3ce2ff4c66bfa6fbcb
Asato TAKAHASHI <asato730@gmail.com>
2024-02-16 01:16:33 +0900

    migrate astro-icon from 0.x to 1.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,58 +1,62 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import markdoc from '@astrojs/markdoc';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import markdoc from "@astrojs/markdoc";
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import remarkCodeTitles from 'remark-code-titles';
-import decapCmsOauth from "astro-decap-cms-oauth";
 import partytown from '@astrojs/partytown';
-import remarkMath from 'remark-math';
+import decapCmsOauth from 'astro-decap-cms-oauth';
+import icon from 'astro-icon';
 import rehypeKatex from 'rehype-katex';
+import remarkCodeTitles from 'remark-code-titles';
+import remarkMath from 'remark-math';
 
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-import expressiveCode, { astroExpressiveCode } from "astro-expressive-code";
+import expressiveCode, { astroExpressiveCode } from 'astro-expressive-code';
 
 // https://astro.build/config
-export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  output: 'server',
-  site: 'https://at-blog.vercel.app',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  server: {
-    // port: 4321, // The port to run the dev server on.
-  },
-  integrations: [
-    astroExpressiveCode(),
-    markdoc(), 
-    svelte(), 
-    tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap(),
-    decapCmsOauth(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"]
-      }
-    }),
-  ],
-  vite: {
-    plugins: [],
-    resolve: {
-      alias: {
-        $: path.resolve(__dirname, './src')
-      }
-    },
-    optimizeDeps: {
-      allowNodeBuiltins: true
-    }
-  },
-  adapter: vercel()
-});
+export default defineConfig(
+	/** @type {import('astro').AstroUserConfig} */ {
+		output: 'server',
+		site: 'https://at-blog.vercel.app',
+		// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+		server: {
+			// port: 4321, // The port to run the dev server on.
+		},
+		integrations: [
+			astroExpressiveCode(),
+			markdoc(),
+			svelte(),
+			tailwind({
+				applyBaseStyles: false,
+			}),
+			icon(),
+			sitemap(),
+			decapCmsOauth(),
+			partytown({
+				config: {
+					forward: ['dataLayer.push'],
+				},
+			}),
+		],
+		vite: {
+			plugins: [],
+			resolve: {
+				alias: {
+					$: path.resolve(__dirname, './src'),
+				},
+			},
+			optimizeDeps: {
+				allowNodeBuiltins: true,
+			},
+		},
+		adapter: vercel(),
+	},
+);
