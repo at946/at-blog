45fcc7cb4f576445b0ae4e69babba4d7662af189
Asato TAKAHASHI <asato730@gmail.com>
2024-03-01 23:33:10 +0900

    create src/data/amazonItems.json

--- src/utils/mdoc/schema/amazon-link.mdoc.ts
+++ src/utils/mdoc/schema/amazon-link.mdoc.ts
@@ -3,7 +3,7 @@ import { component } from '@astrojs/markdoc/config';
 export const amazonLink = {
 	render: component('/src/components/mdoc/AmazonLink.astro'),
 	attributes: {
-		itemId: {
+		asin: {
 			type: String,
 			required: true,
 		},
