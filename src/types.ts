9fc29a61ee8e2295746cdde74f5c94a441a282ba
Asato TAKAHASHI <asato730@gmail.com>
2024-03-01 23:33:10 +0900

    create src/data/amazonItems.json

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,12 @@ export type NavItem = {
 	path: string;
 	title: string;
 };
+
+export type amazonItem = {
+	asin: string;
+	url: string;
+	imageUrl: string;
+	title: string;
+	contributors: string;
+	publisher: string;
+};
