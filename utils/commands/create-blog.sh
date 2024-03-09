48ae910e3e25aada54e9665bcafc74b1f6310ed0
Asato TAKAHASHI <asato730@gmail.com>
2024-03-09 22:49:46 +0900

    created the create:blog command that create a file for blog

---
+++ utils/commands/create-blog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -n "title: "
+read title
+title="${title// /-}"
+directory=src/content/blog/$(date "+%Y%m%d")-$title
+file=$directory/index.mdoc
+mkdir $directory
+touch $file
+cat >$file <<EOL
+---
+title: $title
+tags:
+publicationDate: "$(date "+%Y-%m-%d")"
+isPublished: false
+---
+
+EOL
