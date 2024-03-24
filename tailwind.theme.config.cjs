3f6664ad0ab5b294e97eaf90a6ac777ca3cd65b4
Asato TAKAHASHI <asato730@gmail.com>
2024-03-24 22:10:41 +0900

    added theme.colors in tailwind.config.cjs

--- tailwind.theme.config.cjs
+++ tailwind.theme.config.cjs
@@ -1,21 +1,21 @@
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-    default: {
-        colors: {
-            primary: colors.purple[700],
-            secondary: colors.purple[500],
-            dark: {
-                primary: colors.teal[500],
-                secondary: colors.teal[300]
-            },
-            accent: {
-                gray: {
-                    light: colors.gray[200],
-                    dark: colors.gray[500]
-                },
-                default: colors.blue[700]
-            }
-        }
-    }
-}
+	default: {
+		colors1: {
+			primary: colors.purple[700],
+			secondary: colors.purple[500],
+			dark: {
+				primary: colors.teal[500],
+				secondary: colors.teal[300],
+			},
+			accent: {
+				gray: {
+					light: colors.gray[200],
+					dark: colors.gray[500],
+				},
+				default: colors.blue[700],
+			},
+		},
+	},
+};
