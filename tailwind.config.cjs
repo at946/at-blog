3787cddd0ff2581c85ba65506e72aeb257838d31
Asato TAKAHASHI <asato730@gmail.com>
2024-03-25 23:01:35 +0900

    updated design of blog page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,13 +15,42 @@ module.exports = {
 			dark: {
 				text: colors.gray['200'],
 			},
+			bg: {
+				pre: '#24292e',
+			},
 		},
 		extend: {
 			typography: (theme) => ({
+				dark: {
+					css: {
+						code: {
+							backgroundColor: theme('colors.slate.700'),
+						},
+						'pre code': {
+							backgroundColor: colors.inherit,
+						},
+					},
+				},
 				DEFAULT: {
 					css: {
 						'--tw-prose-body': theme('colors.text'),
+						'--tw-prose-bullets': theme('colors.text'),
 						'--tw-prose-invert-body': theme('colors.dark.text'),
+						'--tw-prose-invert-bullets': theme('colors.dark.text'),
+						code: {
+							backgroundColor: theme('colors.slate.300'),
+							borderRadius: theme('borderRadius.DEFAULT'),
+							paddingTop: theme('spacing.[0.5]'),
+							paddingBottom: theme('spacing.[0.5]'),
+							paddingLeft: theme('spacing.[1.5]'),
+							paddingRight: theme('spacing.[1.5]'),
+						},
+						'code::before': {
+							content: 'none',
+						},
+						'code::after': {
+							content: 'none',
+						},
 					},
 				},
 			}),
