7dbef193a99562f40c5e91e7a2dc34267caf1c67
Asato TAKAHASHI <asato730@gmail.com>
2024-03-29 22:07:30 +0900

    Added python code to update amazonItems.json to scrape the amazon site and find book info by asin.

---
+++ utils/python/scrapingAmazon.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup as bs4
+import requests
+import re
+import json
+
+def main():
+  file_path = '../../src/data/amazonItems.json'
+  amazonItems = []
+  asin = input('ASIN: ')
+
+  with open(file_path) as f:
+    amazonItems = json.load(f)
+
+  for amazonItem in amazonItems:
+    if amazonItem['asin'] == asin:
+      print(f'{asin} already exists.')
+      exit()
+
+  newAmazonItem = getAmazonItem(asin)
+  amazonItems.append(newAmazonItem)
+
+  with open(file_path, 'w') as f:
+    json.dump(amazonItems, f, indent=2, ensure_ascii=False)
+
+def getAmazonItem(asin):
+  url = f'https://www.amazon.co.jp/dp/{asin}?tag=at946-22&linkCode=ogi&th=1&psc=1'
+  res = requests.get(url)
+  print(res)
+  soup = bs4(res.text, 'html.parser')
+
+  newAmazonItem = {'asin': asin}
+  newAmazonItem['url'] = url
+  newAmazonItem['title'] = soup.find('span', {'id': 'productTitle'}).text.strip()
+  newAmazonItem['imageUrl'] = re.sub('_.*_', '_SL500_', soup.find('img', {'id': 'landingImage'})['src'])
+  newAmazonItem['publisher'] = soup.find('div', {'id': 'rpi-attribute-book_details-publisher'}).findChild('div', {'class': 'rpi-attribute-value'}).text.strip()
+  contributors = ''
+  for author in soup.find_all('span', {'class': 'author'}):
+    contributors += author.text.replace('\n','').replace(' (', '(').strip().replace(',',', ')
+  newAmazonItem['contributers'] = contributors
+  return newAmazonItem
+
+if __name__ == "__main__":
+  main()
