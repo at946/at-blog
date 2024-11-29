9606587e3346c8c1185d1e12169edbe6e870a856
at946 <asato730@gmail.com>
2024-11-29 21:55:39 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,12 +20,10 @@ export default defineConfig(
 	/** @type {import('astro').AstroUserConfig} */ {
 		site: 'https://at-blog.vercel.app',
 		output: 'static',
-
 		// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
 		server: {
 			// port: 4321, // The port to run the dev server on.
 		},
-
 		integrations: [
 			astroExpressiveCode(),
 			markdoc(),
@@ -42,7 +40,6 @@ export default defineConfig(
 			}),
 			react(),
 		],
-
 		vite: {
 			plugins: [],
 			resolve: {
