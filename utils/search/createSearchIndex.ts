457e728e3a8a5a6659d64b641a718e3447e54640
at946 <52263024+at946@users.noreply.github.com>
2026-08-09 10:02:35 +0900

    Search (#1515)

---
+++ utils/search/createSearchIndex.ts
@@ -0,0 +1,50 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import fg from 'fast-glob';
+import matter from 'gray-matter';
+import normalizeMarkdoc from '@/utils/normalizeMarkdoc';
+
+type Article = {
+	slug: string;
+	title: string;
+	content: string;
+};
+
+const OUTOUT_FILEPATH: string = 'src/data/search-index.json';
+
+async function createSearchIndex() {
+	const filePaths: string[] = (
+		await fg(`src/content/blog/**/[^_]*.mdoc`)
+	).sort();
+
+	const articles: Article[] = (
+		await Promise.all(
+			filePaths.map(async (filePath) => {
+				const file = await fs.readFile(filePath, 'utf-8');
+				const { data, content } = matter(file);
+				const title: string = data.title;
+				const normalizedContent: string = normalizeMarkdoc(content).trim();
+
+				if (!normalizedContent) {
+					return [];
+				}
+
+				return [
+					{
+						slug: path.basename(path.dirname(filePath)),
+						title: title,
+						content: normalizedContent,
+					},
+				];
+			}),
+		)
+	).flat();
+
+	await fs.writeFile(
+		OUTOUT_FILEPATH,
+		JSON.stringify(articles, null, 2),
+		'utf-8',
+	);
+}
+
+createSearchIndex();
